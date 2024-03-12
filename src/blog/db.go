5633f78a3e7cc3e734f5a5a765f9a7108e6ac0ea
chenwx <chenwx716@gmail.com>
2024-03-12 17:35:56 +0800

    合并冲突

--- src/blog/db.go
+++ src/blog/db.go
@@ -1,27 +1,5 @@
 package blog
 
-<<<<<<< HEAD
-import (
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-)
-
-type WorkDB struct {
-	Dsn string
-}
-
-func (d *WorkDB) GetSession() *gorm.DB {
-	db, err := gorm.Open(mysql.Open(d.Dsn), &gorm.Config{})
-
-	if err != nil {
-		panic(err)
-	}
-	return db
-}
-
-func (d *WorkDB) GetDocPv(url string) int {
-	session := d.GetSession()
-=======
 import dbmysql "git.services.wait/chenwx/cwxgoweb/src/common/dbmysql"
 
 type docDB struct {
@@ -30,7 +8,6 @@ type docDB struct {
 
 func (d *docDB) GetDocPv(url string) int {
 	session := d.db.GetSession()
->>>>>>> main
 	var doc T_doc_access
 
 	return doc.getPv(session, url)
--- src/blog/server.go
+++ src/blog/server.go
@@ -3,32 +3,17 @@ package blog
 import (
 	"errors"
 	"log"
-<<<<<<< HEAD
-
-	// "myblog-api/src/config"
-	// "myblog-api/src/db"
-=======
->>>>>>> main
 	"net/http"
 	net_url "net/url"
 	"regexp"
 	"strconv"
 
-<<<<<<< HEAD
-	"git.services.wait/chenwx/cwxgoweb/src/config"
-	"github.com/gin-gonic/gin"
-)
-
-// 中间件
-func DbMiddleware(db *WorkDB) gin.HandlerFunc {
-=======
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 // 中间件
 func dbMiddleware(db *gorm.DB) gin.HandlerFunc {
->>>>>>> main
 	return func(c *gin.Context) {
 		// 将 db 对象存储到请求上下文中
 		c.Set("db", db)
@@ -40,20 +25,12 @@ func dbMiddleware(db *gorm.DB) gin.HandlerFunc {
 func verifyPathReferer(url string) (string, error) {
 
 	if len(url) <= 0 {
-<<<<<<< HEAD
-		return "", errors.New("Verfiy Referer Path is null")
-=======
 		return "", errors.New("verfiy Referer Path is null")
->>>>>>> main
 	}
 
 	u, err := net_url.Parse(url)
 	if err != nil {
-<<<<<<< HEAD
-		return "", errors.New("Verfiy Referer Path Parse error")
-=======
 		return "", errors.New("verfiy Referer Path Parse error")
->>>>>>> main
 	}
 
 	escape_path := u.EscapedPath()
@@ -64,11 +41,7 @@ func verifyPathReferer(url string) (string, error) {
 	matches := re.FindStringSubmatch(escape_path)
 
 	if len(matches) < 2 {
-<<<<<<< HEAD
-		return "", errors.New("Verfiy Referer Path Re match error")
-=======
 		return "", errors.New("verfiy Referer Path Re match error")
->>>>>>> main
 	}
 
 	return escape_path, nil
@@ -76,11 +49,7 @@ func verifyPathReferer(url string) (string, error) {
 
 func getPv(c *gin.Context) {
 	// 从请求上下文中获取 db 对象
-<<<<<<< HEAD
-	dbObj := c.MustGet("db").(*WorkDB)
-=======
 	dbObj := c.MustGet("db").(*docDB)
->>>>>>> main
 
 	Referer := c.GetHeader("Referer")
 	log.Println(Referer)
@@ -96,11 +65,6 @@ func getPv(c *gin.Context) {
 	c.String(http.StatusOK, strconv.Itoa(num))
 }
 
-<<<<<<< HEAD
-func Server(conf config.ConfBlogApi, confdb config.ConfBlogMysql) {
-
-	db := &WorkDB{Dsn: confdb.Dsn}
-=======
 func Server(cfg BlogConf) {
 
 	if !cfg.Enable {
@@ -114,24 +78,15 @@ func Server(cfg BlogConf) {
 	// db := &docDB{Dsn: cfg.Mysql.Dsn}
 	db := cfg.Mysql.MysqlConf.GetSession()
 
->>>>>>> main
 	r := gin.Default()
 	r.SetTrustedProxies([]string{"127.0.0.1"})
 
 	// 注册中间件
-<<<<<<< HEAD
-	r.Use(DbMiddleware(db))
-
-	r.GET("/api/pv", getPv)
-
-	addr := ":" + conf.Port
-=======
 	r.Use(dbMiddleware(db))
 
 	r.GET("/api/pv", getPv)
 
 	addr := ":" + cfg.Port
->>>>>>> main
 	log.Printf("gin web listen server %s\n", addr)
 	err := r.Run(addr)
 	if err != nil {
